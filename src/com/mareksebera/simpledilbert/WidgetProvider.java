091b1aca68e3c25955591285cf361c32ce56ed00
mareksebera <marek.sebera@gmail.com>
2013-06-28 11:27:32 +0200

    Added widget provider, layout and loader of current image

---
+++ src/com/mareksebera/simpledilbert/WidgetProvider.java
@@ -0,0 +1,63 @@
+package com.mareksebera.simpledilbert;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RemoteViews;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.ImageSize;
+import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
+
+public class WidgetProvider extends AppWidgetProvider {
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+
+		final int widgetCount = appWidgetIds.length;
+		for (int i = 0; i < widgetCount; i++) {
+			int appWidgetId = appWidgetIds[i];
+			updateAppWidget(context, appWidgetManager, appWidgetId);
+		}
+		Log.d("onUpdate", "onUpdate");
+	}
+
+	@Override
+	public void onAppWidgetOptionsChanged(Context context,
+			AppWidgetManager appWidgetManager, int appWidgetId,
+			Bundle newOptions) {
+		Log.d("onAppWidgetOptionsChanged", "onAppWidgetOptionsChanged");
+	}
+
+	@Override
+	public void onEnabled(Context context) {
+		Log.d("onEnabled", "onEnabled");
+		AppController.configureImageLoader(context);
+	}
+
+	static void updateAppWidget(Context context,
+			final AppWidgetManager appWidgetManager, final int appWidgetId) {
+		Log.d("updateAppWidget", "updateAppWidget");
+		final RemoteViews views = new RemoteViews(context.getPackageName(),
+				R.layout.widget_layout);
+
+		final DilbertPreferences prefs = new DilbertPreferences(context);
+		ImageLoader.getInstance().loadImage(prefs.getLastUrl(),
+				new ImageSize(180, 110), new SimpleImageLoadingListener() {
+					@Override
+					public void onLoadingComplete(String imageUri, View view,
+							Bitmap loadedImage) {
+						views.setImageViewBitmap(R.id.widget_image, loadedImage);
+						views.setTextViewText(R.id.widget_title,
+								prefs.getCurrentDate().toString(DilbertPreferences.DATE_FORMATTER));
+						appWidgetManager.updateAppWidget(appWidgetId, views);
+					}
+				});
+
+	}
+}
