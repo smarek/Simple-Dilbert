6ab1867d56d0ad75aad3f357cc19b828d1b5f938
mareksebera <marek.sebera@gmail.com>
2013-07-26 22:44:40 +0200

    New version of UIL library compliancy

--- src/com/mareksebera/simpledilbert/FailSafeBitmapDisplayer.java
+++ src/com/mareksebera/simpledilbert/FailSafeBitmapDisplayer.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.nostra13.universalimageloader.core.assist.LoadedFrom;
 import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
 
 public class FailSafeBitmapDisplayer extends FadeInBitmapDisplayer {
@@ -13,9 +14,9 @@ public class FailSafeBitmapDisplayer extends FadeInBitmapDisplayer {
 	}
 
 	@Override
-	public Bitmap display(Bitmap bitmap, ImageView imageView) {
+	public Bitmap display(Bitmap bitmap, ImageView imageView, LoadedFrom loadedFrom) {
 		try {
-			return super.display(bitmap, imageView);
+			return super.display(bitmap, imageView, loadedFrom);
 		} catch (IllegalStateException e) {
 			Log.e("debug", e.getMessage(), e);
 			return bitmap;
