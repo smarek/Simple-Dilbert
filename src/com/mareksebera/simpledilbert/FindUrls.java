de3b7cb706c8c93b24d3fc70c96759566dbdc58e
mareksebera <marek.sebera@gmail.com>
2013-08-20 13:58:31 +0200

    Keep both methods, stream and allocating full string resource

--- src/com/mareksebera/simpledilbert/FindUrls.java
+++ src/com/mareksebera/simpledilbert/FindUrls.java
@@ -3,6 +3,7 @@ package com.mareksebera.simpledilbert;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.zip.GZIPInputStream;
 
@@ -17,6 +18,29 @@ public final class FindUrls {
 
 	private static final Pattern url_match_pattern = Pattern
 			.compile("/dyn/str_strip(.*).gif");
+	
+	public static List<String> extractUrls(String input) {
+		List<String> result = new ArrayList<String>();
+
+		Pattern pattern = Pattern
+				.compile("\\b(((ht|f)tp(s?)\\:\\/\\/|~\\/|\\/)|www.)"
+						+ "(\\w+:\\w+@)?(([-\\w]+\\.)+(com|org|net|gov"
+						+ "|mil|biz|info|mobi|name|aero|jobs|museum"
+						+ "|travel|[a-z]{2}))(:[\\d]{1,5})?"
+						+ "(((\\/([-\\w~!$+|.,=]|%[a-f\\d]{2})+)+|\\/)+|\\?|#)?"
+						+ "((\\?([-\\w~!$+|.,*:]|%[a-f\\d{2}])+=?"
+						+ "([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)"
+						+ "(&(?:[-\\w~!$+|.,*:]|%[a-f\\d{2}])+=?"
+						+ "([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)*)*"
+						+ "(#([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)?\\b");
+
+		Matcher matcher = pattern.matcher(input);
+		while (matcher.find()) {
+			result.add(matcher.group());
+		}
+
+		return result;
+	}
 
 	public static List<String> extractUrls(HttpResponse response) {
 		List<String> found = new ArrayList<String>();
