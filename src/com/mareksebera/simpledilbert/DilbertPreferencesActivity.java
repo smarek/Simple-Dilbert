9bbe8b9e64685549b918af82ab24cd430531c04f
mareksebera <marek.sebera@gmail.com>
2013-08-20 13:53:51 +0200

    Fixup for sharing preference

--- src/com/mareksebera/simpledilbert/DilbertPreferencesActivity.java
+++ src/com/mareksebera/simpledilbert/DilbertPreferencesActivity.java
@@ -178,7 +178,7 @@ public final class DilbertPreferencesActivity extends SherlockFragmentActivity {
 		preferences.setIsToolbarsHidden(hide_toolbars.isChecked());
 		preferences.setIsHighQualityOn(enable_hq.isChecked());
 		preferences.setIsDarkWidgetLayoutEnabled(force_dark_widget.isChecked());
-		preferences.setIsSharingImage(share_image.isChecked());
+		preferences.setIsSharingImage(!share_image.isChecked());
 		updateWidgets();
 	}
 
