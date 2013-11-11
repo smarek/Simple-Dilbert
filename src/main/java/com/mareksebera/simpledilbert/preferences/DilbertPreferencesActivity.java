85eaec08b4a47838acb2ae27330fe84c8616ef7e
mareksebera <marek.sebera@gmail.com>
2013-11-11 16:05:46 +0100

    Lint fixes

--- src/main/java/com/mareksebera/simpledilbert/preferences/DilbertPreferencesActivity.java
+++ src/main/java/com/mareksebera/simpledilbert/preferences/DilbertPreferencesActivity.java
@@ -34,7 +34,7 @@ public final class DilbertPreferencesActivity extends ActionBarActivity {
 
     private CheckBox force_landscape, enable_hq, force_dark, hide_toolbars,
             force_dark_widget, share_image, mobile_network;
-    private TextView download_path, author;
+    private TextView download_path;
     private DilbertPreferences preferences;
     private static final int REQUEST_DOWNLOAD_TARGET = 1;
     private static final String TAG = "DilbertPreferencesActivity";
@@ -75,7 +75,7 @@ public final class DilbertPreferencesActivity extends ActionBarActivity {
         @Override
         public void onClick(View v) {
             Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                    "mailto","marek@msebera.cz", null));
+                    "mailto", "marek@msebera.cz", null));
             emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Simple Dilbert");
             startActivity(Intent.createChooser(emailIntent, "Simple Dilbert"));
         }
@@ -145,7 +145,7 @@ public final class DilbertPreferencesActivity extends ActionBarActivity {
         share_image = (CheckBox) findViewById(R.id.pref_share_image);
         mobile_network = (CheckBox) findViewById(R.id.pref_mobile_network);
         download_path = (TextView) findViewById(R.id.pref_download_path);
-        author = (TextView) findViewById(R.id.app_author);
+        TextView author = (TextView) findViewById(R.id.app_author);
         LinearLayout download_path_layout = (LinearLayout) findViewById(R.id.pref_download_path_layout);
         TextView license = (TextView) findViewById(R.id.pref_show_license);
         TextView rating = (TextView) findViewById(R.id.pref_rating);
@@ -153,7 +153,6 @@ public final class DilbertPreferencesActivity extends ActionBarActivity {
         license.setOnClickListener(licenseOnClickListener);
         rating.setOnClickListener(ratingOnClickListener);
         author.setOnClickListener(authorOnClickListener);
-
     }
 
     @Override
